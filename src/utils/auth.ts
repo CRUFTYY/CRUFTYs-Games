230f8d03592b52a9214e96bd9e02d0040e8cab22
CRUFTY <100171168+CRUFTYY@users.noreply.github.com>
2025-08-07 22:26:21 -0300

    Updated auth.ts

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,7 +7,7 @@ const AUTHORIZED_EMAILS = [
   'eliascabanillas2006@gmail.com',
   'micaelasf2007@gmail.com',
   'aylasdaiana06@gmail.com',
-  'test@crufty.com',
+  'test@gmail.com',
 ];
 
 const ADMIN_EMAIL = 'eliasmateoet24@gmail.com';
